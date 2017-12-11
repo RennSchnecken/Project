286a038767ecc3581eac8329c7cc8f061387a635
cr4nk89 <thunderdog30000@freenet.de>
2017-12-11 23:22:28 +0100

    Update merge_recMSG.ino

--- MotorSG/merge_recMSG.ino
+++ MotorSG/merge_recMSG.ino
@@ -1,12 +1,16 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
+//globals
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
 char msgString[128];                            // Array to store serial string
 
+//Makro
 #define CAN0_INT 2                              // Set INT to pin 2
+
+//MCP CS auf Pin 10
 MCP_CAN CAN0(10);                               // Set CS to pin 10
 
 
