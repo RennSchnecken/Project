12bf52616c95eab82bbe4a3a41c18916735d53ad
cr4nk89 <thunderdog30000@freenet.de>
2017-12-12 00:02:24 +0100

    Update MotorSG.ino
    ISR CAN RX hinzugefügt

--- MotorSG/MotorSG.ino
+++ MotorSG/MotorSG.ino
@@ -1,29 +1,74 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
+//Makro
+#define CAN0_INT 2        //Interrupt Pin für CAN                         
 
-//Makros
-MCP_CAN CAN0(10);     // Set CS to pin 10
+//CAN0 CS auf Pin 10
+MCP_CAN CAN0(10);         // Set CS to pin 10
+
+//Globale Variable für CAN Botschaft
+volatile byte data[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+INT32U id;
+volatile long unsigned int rxId;
+unsigned char len = 0;
+volatile unsigned char rxBuf[8];
+volatile char msgString[128];      //Array für Datenempfang
+
 
 
 //Setup
-void setup()
+void setup(void)
 {
-  Serial.begin(115200);
+  Serial.begin(115200);   
   Serial.println("Setup Anfang");
 
-  // Initialize MCP2515 running at 8MHz with a baudrate of 50kb/s and the masks and filters disabled.
-  // MCP_ANY(canIDMode):CAN configuration für Tranceiver;  CAN_50KBPS(canSpeed): CAN Geschwindigkeit; MCP_8MHZ(canClock):verbauter Quarz auf Board;
+  //Initialisiere MCP2515 mit 8MHz und 50kb/s.
+  //MCP_ANY(canIDMode):CAN configuration für Tranceiver;  CAN_50KBPS(canSpeed): CAN Geschwindigkeit; MCP_8MHZ(canClock):verbauter Quarz auf Board;
   if(CAN0.begin(MCP_ANY, CAN_50KBPS, MCP_8MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
-    else Serial.println("Fehler bei der Initialisierung des CAN Tranceiver...");
+    else Serial.println("Initialisierungsfehler CAN Tranceiver!");
   CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+  
+  //Pin configuration
+  pinMode(CAN0_INT, INPUT);                            //Pin für CAN receive msg ISR
+  attachInterrupt(digitalPinToInterrupt(CAN0_INT), ISR_recMSG, FALLING);
+
   
   Serial.println("Setup Ende");
 }
 
-//Globale Variable für CAN Botschaft
-byte data[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-INT32U id;
+//Interrupt fuer CAN
+void ISR_recMSG(void)
+{
+  Serial.println("ISR_recMSG() Anfang");    
+  CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    
+  //Pruefen ob standard(11bit) oder extended(29bit) Frame
+  if((rxId & 0x80000000) == 0x80000000)
+    sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);  //speichert standard Frame
+  else
+    sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);            //speichert extended frame
+
+  
+  Serial.print(msgString);  //String ausgeben
+  //Pruefen ob remote request frame
+  if((rxId & 0x40000000) == 0x40000000)
+    {   
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      Serial.print(msgString);
+    } 
+    
+  //Standard MSG
+  else 
+    {
+      for(byte i = 0; i<len; i++){
+        sprintf(msgString, " 0x%.2X", rxBuf[i]);
+        Serial.print(msgString);
+    }     
+   Serial.println("");  
+   Serial.println("ISR_recMSG() Ende");  
+}
+
 
 //Funktion zur Übersichtlichkeit in main()
 //MSG werden nur durch inhalt von data und id bestimmt
@@ -42,7 +87,7 @@ void sendMSG(INT32U id)) //ID für MSG
 }
 
 
-void loop()
+void loop(void)
 {
   data = {0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03};
   sendMSG(0x12);
